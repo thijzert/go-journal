efc6c92e38e8e823b916d63a9bde835355e61000
Thijs van Dijk <github@inurbanus.nl>
2023-01-17 09:05:28 +0100

    Scroll the viewport with the cursor

--- bin/journal-server/assets/js/set-post-body-height.js
+++ bin/journal-server/assets/js/set-post-body-height.js
@@ -4,9 +4,20 @@
 		if ( !textarea ) {
 			return;
 		}
+		const afterInput = document.createElement("A");
+		autogrower.parentElement.insertBefore(afterInput, autogrower.nextSibling);
 
 		textarea.addEventListener("input", (e) => {
 			autogrower.dataset.replicatedValue = textarea.value;
+
+			if ( textarea.selectionEnd && textarea.selectionEnd > textarea.value.length-20 ) {
+				// Cursor is at the end of input. Have some scrolloff space at the bottom of the screen
+				let cr = afterInput.getClientRects()[0];
+
+				if ( cr.y < 0 || cr.y > window.innerHeight ) {
+					afterInput.scrollIntoView(false);
+				}
+			}
 		});
 	}
 })()
