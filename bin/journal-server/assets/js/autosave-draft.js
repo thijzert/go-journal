8bbc31f4ac7f520b6009ed596285adf9c96dfef2
Thijs van Dijk <github@inurbanus.nl>
2023-01-20 17:52:27 +0100

    Add a save status indicator

--- bin/journal-server/assets/js/autosave-draft.js
+++ bin/journal-server/assets/js/autosave-draft.js
@@ -11,6 +11,16 @@ const AUTOSAVE_INTERVAL_MS = 2500;
 		return;
 	}
 
+	const save_status = document.createElement("DIV");
+	const save_time = document.createElement("DIV");
+
+	let indicator = document.querySelector(".indicator-tray .save-status");
+	if ( indicator ) {
+		indicator.innerHTML = '';
+		indicator.appendChild(save_status);
+		indicator.appendChild(save_time);
+	}
+
 	let this_url = new URL(location.href);
 	let draft_url = new URL("journal/draft", location.href);
 	for ( let k of this_url.searchParams.keys() ) {
@@ -19,19 +29,29 @@ const AUTOSAVE_INTERVAL_MS = 2500;
 
 	let draft_id = "";
 	let save_draft = async () => {
+		save_status.innerText = "Autosaving...";
 		try {
 			let pb = new FormData();
 			pb.set("draft_id", draft_id);
 			pb.set("body", ipt_body.value);
+			let draft_emptied = (ipt_body.value.trim() === '');
 			let q = await fetch(draft_url, {method: "POST", body: pb});
 			q = await q.json();
 			if ( q.ok == 1 ) {
-				console.log("draft saved", q);
+				save_status.innerText = "Draft saved";
+				save_time.innerText = `Last saved at ${(new Date()).toTimeString().slice(0,5)}`;
 				draft_id = q.draft_id;
+
+				if ( draft_emptied ) {
+					save_status.innerText = "Draft deleted";
+					save_time.innerText = "";
+				}
 			} else {
+				save_status.innerText = "Error saving";
 				console.error("unable to save draft", q);
 			}
 		} catch (e) {
+			save_status.innerText = "\u274c Error saving";
 			console.error(e);
 			throw e;
 		}
